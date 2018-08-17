f96fc7fe175ac5c55ba715793376081a04641ad6
Jesper Orb <jesper.orb@zocom.se>
2018-08-17 08:12:02 +0200

    Refactor const into function in utils

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-useless-escape */
 
-export const validateEmail = inputEmail => {
+export function validateEmail(inputEmail){
   const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   return emailRegex.test(inputEmail);
 };
 
-export const validatePassword = inputPassword => {
+export function validatePassword(inputPassword) {
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
   return passwordRegex.test(inputPassword);
 };
 
-export const validateLogin = (email, password) => {
+export function validateLogin(email, password){
   if(validateEmail(email) && validatePassword(password)){
     return true;
   }
